b01ce20db01855f4e953bf0b7994f16c66f8120c
subhadippal66 <subhadippal66@gmail.com>
2021-01-24 23:25:56 +0530

    update 2 Homepage'

--- src/Home.js
+++ src/Home.js
@@ -8,17 +8,36 @@ function Home() {
     <div className="home">
       <img className="home_banner" src={Banner_1} alt="Banner 1" />
       <div className="product_container">
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-      </div>
-      <div className="product_container">
-        <Product />
-        <Product />
-      </div>
-      <div className="product_container">
-        <Product />
+        {/* <Product
+          title="Redmi 9A (Nature Green, 2GB Ram, 32GB Storage)"
+          image="https://images-na.ssl-images-amazon.com/images/I/710fuhtgISL._SL1500_.jpg"
+          price="6,999.00"
+          rating="4"
+        /> */}
+        <Product
+          title="Samsung Galaxy M01 Core (Black, 2GB RAM, 32GB Storage)"
+          image="https://images-na.ssl-images-amazon.com/images/I/71AYb2AGHXL._SL1500_.jpg"
+          price={5999.0}
+          rating={4}
+        />
+        <Product
+          title="POLESTAR Noble Blue Casual bagpack/School Bag/Laptop Backpack"
+          image="https://images-na.ssl-images-amazon.com/images/I/91SOSQyJXPL._SL1500_.jpg"
+          price={399.0}
+          rating={4}
+        />
+        <Product
+          title="The Alchemist"
+          image="https://images-na.ssl-images-amazon.com/images/I/410llGwMZGL._SX328_BO1,204,203,200_.jpg"
+          price={173.0}
+          rating={4}
+        />
+        <Product
+          title="Fossil Grant Chronograph Analog Black Dial Men's Watch - FS4832"
+          image="https://images-na.ssl-images-amazon.com/images/I/71GhdYtz8cL._UL1500_.jpg"
+          price={7095.0}
+          rating={4}
+        />
       </div>
     </div>
   );
--- src/Product.js
+++ src/Product.js
@@ -1,27 +1,20 @@
 import React from "react";
 import "./Product.css";
 
-function Product() {
+function Product({ title, image, price, rating }) {
   return (
     <div className="product">
       <div className="product_info">
-        <p>
-          Nokia 5.3 Android One Smartphone with Quad Camera, 4 GB RAM and 64 GB
-          Storage - Charcoal
-        </p>
+        <p>{title}</p>
         <p className="price">
           <small>₹</small>
-          <strong>11,498.00 </strong>
+          <strong>{price} </strong>
         </p>
         <div className="product_rating">
           <p>⭐</p>
         </div>
       </div>
-      <img
-        className="product_image"
-        src="https://images-na.ssl-images-amazon.com/images/I/61L1ItFgFHL._SL1500_.jpg"
-        alt="product image"
-      />
+      <img className="product_image" src={image} alt="product image" />
       <button>Add To Basket</button>
     </div>
   );
