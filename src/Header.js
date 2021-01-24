f99674315b75cfae6fe17c26eef7f7f9e5129b5e
subhadippal66 <subhadippal66@gmail.com>
2021-01-24 19:20:27 +0530

    first Push after createing react app

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+//rfce
+
+import React from 'react'
+import './Header.css'
+
+function Header() {
+    return (
+        <div className='header'>
+            
+        </div>
+    )
+}
+
+export default Header
